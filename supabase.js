b77e5e38ceb6699cbf0f7af1929b3ec05f575d96
haleemmamdouh2-art <haleemmamdouh2@gmail.com>
2026-04-28 05:18:36 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // Initialize Supabase Client
 
-const supabaseUrl = 'https://byqfgirtizfubmvrkuts.supabase.co'; 
+const supabaseUrl = 'https://byqfgirtizfvbmvrkuts.supabase.co'; 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5cWZnaXJ0aXpmdmJtdnJrdXRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMjE0MjksImV4cCI6MjA5Mjg5NzQyOX0.KIh1RSlwO0ps4vT4tFBvB4qiamCxLyLhCtUvaVnYUvY';
 
 // We wrap this in a try/catch so the app doesn't crash if the URL is invalid
